63f2c477d1400c6ef3f156af2da0a92ecfb0b211
zwhitman <zwhitmandeloitte@gmail.com>
2016-02-15 13:22:34 -0500

    added search functions

---
+++ js/search.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * Created by zwhitman on 2/15/2016.
+ */
+
+//socrata
+var baseURL = 'https://dot-hackathon.data.socrata.com/',
+    qParam = 'browse?q=';
+
+//openData
+//var baseURL = 'http://celebratingcities.deloittegeo.opendata.arcgis.com/',
+//    qParam = 'datasets?q=';
+
+function search(input) {
+    if(typeof input === 'undefined'){
+        var val = document.getElementById('s').value;
+        if(val==''){
+            window.location = baseURL;
+        } else{
+            window.location = baseURL + qParam + val;
+        }
+    } else if(input==0){
+        return
+    } else {
+        window.location = baseURL + qParam + input;
+    }
+}
