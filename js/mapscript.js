749ae273614366517c226c7f73fa2c904a436fad
zwhitman <zwhitmandeloitte@gmail.com>
2016-02-16 17:00:34 -0500

    copy fix and map fallback

--- js/mapscript.js
+++ js/mapscript.js
@@ -9,17 +9,38 @@ function getRandomInt(min, max) {
 
 
 var centers = [[-77.038,38.915],[-76.61,39.31]],
-    centerChoice = centers[getRandomInt(0,1)];
+    centersBU = [[38.915,-77.038],[39.31,-76.61]],
+    centerChoice = centers[getRandomInt(0,1)],
+    centerChoiceBU = centersBU[getRandomInt(0,1)],
+    centerAssign;
 
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiendoaXRtYW4iLCJhIjoiY2lqcjhwZjVrMDgwcXV5bHh0bmN5eWtxdyJ9.FQ-Fd-X4W08OL7i7ALPmxA';
-var map = new mapboxgl.Map({
-    container: 'map-one', // container id
-    style: 'mapbox://styles/zwhitman/cijr8l1pu006990lx8cm0km52', //hosted style id
-    center: centerChoice, // starting position
-    zoom: 11.5,// starting zoom
-    interactive: false
-});
+
+try{
+    mapboxgl.accessToken = 'pk.eyJ1IjoiendoaXRtYW4iLCJhIjoiY2lqcjhwZjVrMDgwcXV5bHh0bmN5eWtxdyJ9.FQ-Fd-X4W08OL7i7ALPmxA';
+    var map = new mapboxgl.Map({
+        container: 'map-one', // container id
+        style: 'mapbox://styles/zwhitman/cijr8l1pu006990lx8cm0km52', //hosted style id
+        center: centerChoice, // starting position
+        zoom: 11.5,// starting zoom
+        interactive: false
+    });
+    centersAssign = centers;
+} catch(err){
+// Map configuration settings
+    console.log(err);
+    L.mapbox.accessToken = 'pk.eyJ1IjoiendoaXRtYW4iLCJhIjoiY2lqcjhwZjVrMDgwcXV5bHh0bmN5eWtxdyJ9.FQ-Fd-X4W08OL7i7ALPmxA';
+    var mbUrl = 'https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png';
+    var basemap = L.tileLayer(mbUrl, {id: 'zwhitman.l2on037l'});
+    var map = L.map('map-one', {
+        center: centerChoiceBU,
+        zoom: 12,
+        //maxBounds: bounds,
+        layers: [basemap],
+        interactive: false
+    });
+    centersAssign = centersBU;
+}
 
 $(window).bind('scroll', function (e) {
     parallaxScroll();
@@ -31,8 +52,7 @@ function parallaxScroll() {
     $('#banner-head').css('top', (0 - (scrolled * .28)) + 'px');
 }
 
-
 function pan(city){
-    map.panTo(centers[city])
+    map.panTo(centersAssign[city])
 }
 
